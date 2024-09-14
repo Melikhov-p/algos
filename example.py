49eef938015561b6aa01d21cb58418ee645ef9c2
Melikhov-p <melikhov.pavel@gmail.com>
2024-09-14 14:57:24 +0300

    add README.md and queue structure

--- example.py
+++ example.py
@@ -1,18 +1,26 @@
 from Stack import Stack
+from Queue import Queue
 
 
 def main():
-    stack = Stack()
-    print(stack.is_empty())
+    # stack = Stack()
+    # print(stack.is_empty())
+    #
+    # stack.push(1)
+    # stack.push(2)
+    # stack.push(3)
+    #
+    # print(stack.is_empty())
 
-    stack.push(1)
-    stack.push(2)
-    stack.push(3)
+    queue = Queue()
+    print(queue.is_empty())
 
-    print(stack.is_empty())
-    print(stack.peek())
-    print(stack.pop())
-    print(stack.size())
+    queue.enqueue(1)
+    queue.enqueue(2)
+    queue.enqueue(3)
+    print(queue.is_empty())
+
+    print(queue.dequeue())
 
 
 if __name__ == "__main__":
